5363d70a8fd091b7356ea1bb37c6441778834383
Stive627 <fossistive627@gmail.com>
2025-01-27 02:59:26 +0530

    information

--- consumeIp.js
+++ consumeIp.js
@@ -7,23 +7,23 @@ const consumeIP = (req, res) => {
         var urlWeather = `http://api.openweathermap.org/data/2.5/weather?`+`lat=${value.lat}&lon=${value.lon}&appid=${YOUR_ACCESS_KEY}`
         fetchUrl(urlWeather,(val) => {
             try {
-                // const information = {
-                //     "temp":val.main.temp,
-                //     "feels_like": val.main.temp_max,
-                //     "temp_min":val.main.temp_min,
-                //     "temp_max":val.main.temp_max,
-                //     "description":val.weather[0].description,
-                //     "pressure":val.main.pressure,
-                //     "humidity":val.main.humidity,
-                //     "visibility":val.visibility * 0.001,
-                //     "wind_speed":val.wind.speed,
-                //     "country":val.sys.country,
-                //     "sunrise":time(val.sys.sunrise),
-                //     "sunset":time(val.sys.sunset),
-                //     "name":val.name,
-                //     "timezone": (Math.floor(val.timezone / 3600)).toFixed(1)
-                // } 
-            return res.status(200).send({...val.main, ...val.sys})
+                const information = {
+                    "temp":val.main.temp,
+                    "feels_like": val.main.temp_max,
+                    "temp_min":val.main.temp_min,
+                    "temp_max":val.main.temp_max,
+                    "description":val.weather[0].description,
+                    "pressure":val.main.pressure,
+                    "humidity":val.main.humidity,
+                    "visibility":val.visibility * 0.001,
+                    "wind_speed":val.wind.speed,
+                    "country":val.sys.country,
+                    "sunrise":time(val.sys.sunrise),
+                    "sunset":time(val.sys.sunset),
+                    "name":val.name,
+                    "timezone": (Math.floor(val.timezone / 3600)).toFixed(1)
+                } 
+            return res.status(200).send(information)
             } catch (error) {
                 res.status(400).send(error)
                 console.log(error)
