648c8d440a5f41dbb6e9488b4721def720d82b78
Stive627 <fossistive627@gmail.com>
2025-01-27 03:08:12 +0530

    temp

--- consumeIp.js
+++ consumeIp.js
@@ -1,4 +1,5 @@
 const fetchUrl = require("./fetchUrl")
+const time = (timezone) => new Intl.DateTimeFormat('en-US', {'hour':'2-digit', 'minute':'2-digit'}).format(new Date(timezone))
 const consumeIP = (req, res) => {
     var ipAddress = req.ip
     var urllocation = `http://ip-api.com/json/${ipAddress}`
@@ -7,26 +8,26 @@ const consumeIP = (req, res) => {
         var urlWeather = `http://api.openweathermap.org/data/2.5/weather?`+`lat=${value.lat}&lon=${value.lon}&appid=${YOUR_ACCESS_KEY}`
         fetchUrl(urlWeather,(val) => {
             try {
-                const information = {
-                    "temp":val.main.temp,
-                    "feels_like": val.main.temp_max,
-                    "temp_min":val.main.temp_min,
-                    "temp_max":val.main.temp_max,
-                    "description":val.weather[0].description,
-                    "pressure":val.main.pressure,
-                    "humidity":val.main.humidity,
-                    "visibility":val.visibility * 0.001,
-                    "wind_speed":val.wind.speed,
-                    "country":val.sys.country,
-                    "sunrise":time(val.sys.sunrise),
-                    "sunset":time(val.sys.sunset),
-                    "name":val.name,
-                    "timezone": (Math.floor(val.timezone / 3600)).toFixed(1)
-                } 
-            return res.status(200).send(information)
+                // const information = {
+                //     "temp":val.main.temp,
+                //     "feels_like": val.main.temp_max,
+                //     "temp_min":val.main.temp_min,
+                //     "temp_max":val.main.temp_max,
+                //     "description":val.weather[0].description,
+                //     "pressure":val.main.pressure,
+                //     "humidity":val.main.humidity,
+                //     "visibility":val.visibility * 0.001,
+                //     "wind_speed":val.wind.speed,
+                //     "country":val.sys.country,
+                //     "sunrise":time(val.sys.sunrise),
+                //     "sunset":time(val.sys.sunset),
+                //     "name":val.name,
+                //     "timezone": (Math.floor(val.timezone / 3600)).toFixed(1)
+                // } 
+            return res.status(200).send(val.main.temp)
             } catch (error) {
-                res.status(400).send(error)
-                console.log(error)
+                res.status(400).send(error.response)
+                console.log(error.response)
             }
         })
     })
