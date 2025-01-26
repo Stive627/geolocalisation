61817ccbe0cd7d42cde519132c41486fe3d94f09
Stive627 <fossistive627@gmail.com>
2025-01-26 23:27:41 +0530

    26-01-2025 four

--- consumeIp.js
+++ consumeIp.js
@@ -1,8 +1,9 @@
-const consumeIP = async (req, res) => {
+const consumeIP = (req, res) => {
     var ipAddress = req.ip
-     const detail = await fetch(`http://ip-api.com/json/${ipAddress}`)
-    if(detail.ok){
-        return res.status(200).send(detail.json())
-    }
+    fetch(`http://ip-api.com/json/${ipAddress}`)
+    .then((value) =>value.json())
+    .then((value) => res.status(200).send(value))
+    .catch((err) => res.status(400).send(`An error occured\n${err}`))
 }
+
 module.exports = consumeIP
