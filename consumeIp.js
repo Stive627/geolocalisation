b4999f098f5cdf3032caf50ddbacb43c9a56d4a4
Stive627 <fossistive627@gmail.com>
2025-01-26 23:09:38 +0530

    26-01-2025 second

--- consumeIp.js
+++ consumeIp.js
@@ -1,6 +1,8 @@
 const consumeIP = (req, res) => {
-    console.log(req.ip)
-    return res.status(200).send(req.ip)
+    var ipAddress = req.ip
+    fetch(`http://ip-api.com/json/${ipAddress}`)
+    .then((value) =>value.json())
+    .then((value) => res.status(200).send(value))
+    .catch((err) => res.status(400).send(`An error occured\n${err}`))
 }
-
 module.exports = consumeIP
