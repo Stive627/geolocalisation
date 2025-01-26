f01211ad1d455fd6ecdb6aa81a95dc5de2b35401
Stive627 <fossistive627@gmail.com>
2025-01-27 00:09:51 +0530

    last weather

--- consumeIp.js
+++ consumeIp.js
@@ -5,9 +5,9 @@ const consumeIP = (req, res) => {
     var urllocation = `http://ip-api.com/json/${ipAddress}`
     var YOUR_ACCESS_KEY = 'e9e6d72748bba48c0342882ec1dd5da2'
     fetchUrl(urllocation, (value) => {
-        // var urlWeather = `http://api.openweathermap.org/data/2.5/weather?`+`lat=${value.latitude}&lon=${value.longitude}&appid=${YOUR_ACCESS_KEY}`
-        // fetchUrl(urlWeather, (value) => res.status(200).send(value))
-        return res.status(200).send(value)
+        var urlWeather = `http://api.openweathermap.org/data/2.5/weather?`+`lat=${value.lat}&lon=${value.lon}&appid=${YOUR_ACCESS_KEY}`
+        fetchUrl(urlWeather, (value) => res.status(200).send(value))
+        // return res.status(200).send(value)
     })
 }
 module.exports = consumeIP
